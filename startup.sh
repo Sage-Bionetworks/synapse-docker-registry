f2d0bd959699272dd85b6533550ceab5e51c3496
bhoff <bruce.hoff@sagebase.org>
2024-09-25 15:58:00 -0700

    PLFM-8554: updates from CR

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Inject notification listener authorization credentials into config.yml
+# The value is taken from the environment variable, `notification_auth` which,
+# during ECS deployment comes from the AWS Secrets Manager.
 sed -i "s/notification_auth/$notification_auth/g" /etc/docker/registry/config.yml
 
 # this assumed a particular start-up for the container registry
